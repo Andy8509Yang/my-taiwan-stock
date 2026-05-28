26660a9636eff0118e0be2f234cc187b9a223c06
Andy8509Yang <andy40324032@gmail.com>
2026-05-29 00:17:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.write("輸入股票代號，自動產生含均線、成交量的專業 K 線�
 # 側邊欄：使用者輸入區
 st.sidebar.header("⚙️ 設定參數")
 stock_id = st.sidebar.text_input("請輸入台股代號：", "2330")
-period = st.sidebar.selectbox("請選擇時間區間：", ["3個月", "6個月", "1年", "1個月"])
+period = st.sidebar.selectbox("請選擇時間區間：", ["1個月","3個月", "6個月", "1年",])
 
 # 轉換時間格式給 yfinance
 period_map = {"1個月": "1mo", "3個月": "3mo", "6個月": "6mo", "1年": "1y"}
